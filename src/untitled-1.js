ca241871fe4eef27047786fb3f0e53ff2d4d2886
susu-git92 <chlojeong@gmail.com>
2020-11-24 10:20:59 +0900

    1차 공부

--- src/untitled-1.js
+++ src/untitled-1.js
@@ -4,22 +4,44 @@ function sayHi() {
   return 'world' //was never executed
 }
 
-const result = sayHi()
-console.log(result)   
-
-function sayHiGenerator(params) {
-  yield 'hey'
-  return 'hi'
-}
+const result = sayHi() {}
+console.log(result)  //hi 
 
 const resultGenerator = sayHiGenerator()
-console.log(resultGenerator.next()) { [Iterator] }
+// console.log(resultGenerator.next()) { [Iterator] }
+console.log(result) //hi
+
 
 function* sayHiGenerator() {
   yield 'hey'
+  yield 'world'
+  yield 'guys'
+  //statements
   return 'hi'
 }
 
 
 const resultGenerator = sayHiGenerator()
 console.log(resultGenerator.next())   //{value: 'hey', done: false}
+console.log(resultGenerator.next()) //{value: 'world', done: true}
+console.log(resultGenerator.next()) //{value: 'guys', done:true}
+
+const resultGeneratorForOf = sayHiGenerator()
+
+for (const iterator of resultGeneratorForOf) {
+  console.log(iterator) //hey, world, guys
+}
+
+function* newGenerator() {
+  yield 'something'
+  const final = yield 'give me value'
+  return final
+}
+
+const newGeneratorIt = newGenerator()
+
+console.log(newGeneratorIt.next()) //{value: 'something' done: false}
+console.log(newGeneratorIt.next())//{value: 'final value' done: false}
+console.log(newGeneratorIt.next('give me value'))//{value: 'undefined' done: false}
+
+ 
